fa5679adcf656a87d6958117c1b79794765e6a36
kayos@tcp.direct <kayos@tcp.direct>
2021-09-21 05:14:04 -0700

    Tidy up documentation

--- models.go
+++ models.go
@@ -7,25 +7,25 @@ import (
 )
 
 const (
-	// DefaultWindow is the standard window of time ratelimit triggers are observed in seconds
+	// DefaultWindow is the standard window of time ratelimit triggers are observed in seconds.
 	DefaultWindow = 25
-	// DefaultBurst is the standard amount of triggers observed within Window before ratelimiting occurs
+	// DefaultBurst is the standard amount of triggers observed within Window before ratelimiting occurs.
 	DefaultBurst = 25
 )
 
 var debugChannel chan string
 
-// Identity is an interface that allows any arbitrary type to be used for a unique key in ratelimit checks when implemented */
+// Identity is an interface that allows any arbitrary type to be used for a unique key in ratelimit checks when implemented.
 type Identity interface {
 	UniqueKey() string
 }
 
-// Limiter implements an Enforcer to create an arbitrary ratelimiter
+// Limiter implements an Enforcer to create an arbitrary ratelimiter.
 type Limiter struct {
 	Source Identity
-	// Patrons are the IRC users that we are rate limiting
+	// Patrons are the IRC users that we are rate limiting.
 	Patrons *cache.Cache
-	// Ruleset is the actual ratelimitting model
+	// Ruleset is the actual ratelimitting model.
 	Ruleset Policy
 	/* Debug mode (toggled here) enables debug messages
 	delivered through a channel. See: DebugChannel() */
@@ -36,15 +36,12 @@ type Limiter struct {
 	mu    *sync.RWMutex
 }
 
-// Policy defines the mechanics of our ratelimiter
+// Policy defines the mechanics of our ratelimiter.
 type Policy struct {
-	// Window defines the duration in seconds that we should keep track of ratelimit triggers
+	// Window defines the duration in seconds that we should keep track of ratelimit triggers,
 	Window int
-	/* Burst is the amount of times that Check will not trigger a limit
-	within the duration defined by Window */
+	// Burst is the amount of times that Check will not trigger a limit within the duration defined by Window.
 	Burst int
-	/* Strict mode punishes triggers of the ratelimit
-	by increasing the amount of time they have to wait
-	every time they trigger the limitter */
+	// Strict mode punishes triggers of the ratelimitby increasing the amount of time they have to wait every time they trigger the limitter.
 	Strict bool
 }
